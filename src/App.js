e874f56ff9bf5d062ec012b368e797b3f25a0409
Mehedi Haque <kpopdw@gmail.com>
2020-04-23 13:31:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import { ClearButton } from "./components/ClearButton";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const addToInput = (val) => {
+    setInput({ input: input + val });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="calc-wrapper">
+        <Input input={input}></Input>
+        <div className="row">
+          <Button handleClick={addToInput}>7</Button>
+          <Button handleClick={addToInput}>8</Button>
+          <Button handleClick={addToInput}>9</Button>
+          <Button handleClick={addToInput}>/</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={addToInput}>4</Button>
+          <Button handleClick={addToInput}>5</Button>
+          <Button handleClick={addToInput}>6</Button>
+          <Button handleClick={addToInput}>X</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={addToInput}>1</Button>
+          <Button handleClick={addToInput}>2</Button>
+          <Button handleClick={addToInput}>3</Button>
+          <Button handleClick={addToInput}>+</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={addToInput}>.</Button>
+          <Button handleClick={addToInput}>0</Button>
+          <Button>=</Button>
+          <Button>-</Button>
+        </div>
+        <div className="row">
+          <ClearButton handleClear={() => setInput({ input })}>
+            Clear
+          </ClearButton>
+        </div>
+      </div>
     </div>
   );
 }
